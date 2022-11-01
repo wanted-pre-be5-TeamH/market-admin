06990d3d608cb8b11c34b73addec5a41ce41d8c4
rojiwon0325 <bond9986@naver.com>
2022-11-01 22:14:22 +0900

    [CouponType] Domain 구현 & AOP 구현

--- src/api/coupon-type/domain/coupon-type.aggregte.ts
+++ src/api/coupon-type/domain/coupon-type.aggregte.ts
@@ -5,10 +5,57 @@ export class CouponType
   extends BaseAggregate<CouponTypeDomain.Id>
   implements CouponTypeDomain.Static<typeof CouponType>
 {
-  flat_price: number;
-  discount_price: number;
-  discount_rate: number;
-  static get: (props: CouponTypeDomain.Props) => CouponTypeDomain.Aggregate;
-  getResponse: () => CouponTypeDomain.Response;
-  caculateDiscountedPrice: (price: number) => number;
+  constructor(
+    id: CouponTypeDomain.Id,
+    created_at: Date,
+    updated_at: Date,
+    readonly flat_price: number,
+    readonly discount_price: number,
+    readonly discount_rate: number,
+  ) {
+    super(id, created_at, updated_at);
+  }
+
+  static get(props: CouponTypeDomain.Props): CouponTypeDomain.Aggregate {
+    const {
+      id,
+      created_at,
+      updated_at,
+      flat_price,
+      discount_price,
+      discount_rate,
+    } = props;
+    const now = new Date();
+    return new CouponType(
+      id ?? 0,
+      created_at ?? now,
+      updated_at ?? now,
+      flat_price,
+      discount_price,
+      discount_rate,
+    );
+  }
+
+  static calculateDiscountedPrice(
+    price: number,
+    coupon: Pick<
+      CouponTypeDomain.Property,
+      'flat_price' | 'discount_price' | 'discount_rate'
+    >,
+  ): number {
+    const { flat_price, discount_price, discount_rate } = coupon;
+    let discounted = price;
+    if (discounted >= flat_price) {
+      discounted = (discounted - discount_price) * discount_rate;
+      if (discounted < 0) {
+        discounted = 0;
+      }
+    }
+    return discounted;
+  }
+
+  getResponse(): CouponTypeDomain.Response {
+    const { id, flat_price, discount_price, discount_rate } = this;
+    return { id, flat_price, discount_price, discount_rate };
+  }
 }
